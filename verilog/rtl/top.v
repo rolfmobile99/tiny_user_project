29dce52b37b67142942e6419185952e83ad45b12
rolf widenfelt <rolfvw@gmail.com>
2022-12-04 00:32:02 -0800

    typo

--- verilog/rtl/top.v
+++ verilog/rtl/top.v
@@ -13,7 +13,7 @@ module rolfmobile99_top(
   wire clk = io_in[7];      // notused
   wire reset = io_in[6];    // notused
   wire xnor_a = io_in[5];
-  wire nxor_b = io_in[4];
+  wire xnor_b = io_in[4];
   wire [3:0] notused_in = io_in[3:0];
 
   assign io_out[7] = xnor_y;
